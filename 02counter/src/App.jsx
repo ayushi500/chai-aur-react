7f5bff5493df54aa2a37f1e52a4d1e313e31f3bc
ayushi500 <ayusharma517@gmail.com>
2025-11-03 14:17:16 +0530

    interview

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -12,9 +12,34 @@ function App() {
 
   const addValue=()=>{
     
-    counter++;
-    if(counter<=20){  //stop here if counter > 0
-    setCounter(counter)
+    //counter++;
+    if(counter<=20){  //stop here if counter > 20
+      
+    setCounter((prevCounter)=>prevCounter+1)   //setCounter((prevCounter)=>{})->setCounter returns a call back
+    setCounter((prevCounter)=>prevCounter+1)   //setCounter((prevCounter)=>{})->setCounter returns a call back
+    setCounter((prevCounter)=>prevCounter+1)   //setCounter((prevCounter)=>{})->setCounter returns a call back
+    setCounter((prevCounter)=>prevCounter+1)   //setCounter((prevCounter)=>{})->setCounter returns a call back
+    setCounter((prevCounter)=>prevCounter+1)   //setCounter((prevCounter)=>{})->setCounter returns a call back
+    /*prevCounter is the last updated state
+     jo call back ke through aata hai,wha pe complete execution 
+     hone ke baad hi doosra call hota hai and wha pe proper update hota hai
+    */
+
+
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1) 
+
+     /** Interview Question
+      * 
+    Agr humne setCounter() function baar-baar call kr diya to value 5 ke baad kya ho jaayegi means btn click krte 
+    hi value 6 hogi,7 hogi,8 hogi,9 hogi,ya 10 hojaayegi? 
+
+    only become 6=>value by 1 hi increase hogi addValue() btn click krne se
+    NEECHE DEKHO
+    */
+    
     }
   }
   const removeValue=()=>{
@@ -46,3 +71,59 @@ update ho rhi hai,main UI me ho hi nhi rhi hai,so here we use hooks
 
 ** UI updation ko react control krta hai
 */
+
+
+/**INTERVIEW QUESTION
+ * 
+ * 
+ *
+ When React runs these lines,
+it doesn’t update the value right away each time.
+It remembers all these updates ,it collect them in batches and applies them together later.
+
+But — every line still sees the old value (5).
+
+So it’s like:
+
+setCounter(5 + 1)
+setCounter(5 + 1)
+setCounter(5 + 1)
+setCounter(5 + 1)
+setCounter(5 + 1)
+
+React applies the last one → result = 6 ✅
+(so only +1, not +5)
+
+
+if you want it to increase 5 times:
+
+You must tell React to use the latest value each time, like this:
+setCounter(prev => prev + 1)
+setCounter(prev => prev + 1)
+setCounter(prev => prev + 1)
+setCounter(prev => prev + 1)
+setCounter(prev => prev + 1)
+
+5 → 6 → 7 → 8 → 9 → 10
+
+
+
+When multiple setState (or setCounter) calls use the same state
+variable, React batches them and uses the same old value(5) i.e it does not update counter immediately.,  
+i.e counter is still 5 inside this same function.So the final update happens only once.
+  To ensure each update uses the latest value, use the functional 
+  form of setState — setCounter(prev => prev + 1).
+ */
+
+
+
+
+  /**
+   * useState() jitne bhi updates bhejne hain UI me,unko batches me bhejta hai
+   * //setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1) 
+    they are updating the same counter and saara jo counter update ho rha hai,wo same hi to 
+    kaam hi kr rha hai
+   */
